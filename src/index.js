84c2b946bf74d0d8eb7966fe537f250ef504b332
Liam O'Callaghan <liam@liamocallaghan.com>
2020-07-22 06:23:01 -0400

    use eslint

--- src/index.js
+++ src/index.js
@@ -1,42 +1,34 @@
-import React from "react";
-import {
-  Formik,
-  Field,
-  Form,
-  FieldArray
-} from "formik";
+import React from 'react'
+import { Formik, Field, Form, FieldArray } from 'formik'
 import {
   TextField,
   Button,
   Checkbox,
   Select,
   MenuItem
-} from "@material-ui/core";
+} from '@material-ui/core'
 
-import CssBaseline from '@material-ui/core/CssBaseline';
+import CssBaseline from '@material-ui/core/CssBaseline'
 import Paper from '@material-ui/core/Paper'
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
 
-import * as yup from "yup";
+import * as yup from 'yup'
 
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles'
 
 const validationSchema = yup.object({
-  firstName: yup
-    .string()
-    .required()
-    .max(10),
+  firstName: yup.string().required().max(10),
   pets: yup.array().of(
     yup.object({
       name: yup.string().required()
     })
   )
-});
+})
 
 const useStyles = makeStyles((theme) => ({
   appBar: {
-    position: 'relative',
+    position: 'relative'
   },
   layout: {
     width: 'auto',
@@ -45,8 +37,8 @@ const useStyles = makeStyles((theme) => ({
     [theme.breakpoints.up(600 + theme.spacing(2) * 2)]: {
       width: 600,
       marginLeft: 'auto',
-      marginRight: 'auto',
-    },
+      marginRight: 'auto'
+    }
   },
   paper: {
     marginTop: theme.spacing(3),
@@ -55,109 +47,109 @@ const useStyles = makeStyles((theme) => ({
     [theme.breakpoints.up(600 + theme.spacing(3) * 2)]: {
       marginTop: theme.spacing(6),
       marginBottom: theme.spacing(6),
-      padding: theme.spacing(3),
-    },
+      padding: theme.spacing(3)
+    }
   },
   stepper: {
-    padding: theme.spacing(3, 0, 5),
+    padding: theme.spacing(3, 0, 5)
   },
   buttons: {
     display: 'flex',
-    justifyContent: 'flex-end',
+    justifyContent: 'flex-end'
   },
   button: {
     marginTop: theme.spacing(3),
-    marginLeft: theme.spacing(1),
-  },
-}));
-
-
+    marginLeft: theme.spacing(1)
+  }
+}))
 
 export const DatsCreatorGui = () => {
-
-  const classes = useStyles();
+  const classes = useStyles()
 
   return (
-
     <React.Fragment>
       <CssBaseline />
       <div className={classes.layout}>
         <Paper className={classes.paper}>
-          <Typography component="h1" variant="h4" align="center">
+          <Typography component='h1' variant='h4' align='center'>
             Create DATS.json
           </Typography>
           <Formik
-            validateOnChange={true}
+            validateOnChange
             initialValues={{
-              title: "",
-              description: "",
+              title: '',
+              description: '',
               types: [],
-              license: "",
+              license: '',
               keywords: [],
               creators: []
             }}
             validationSchema={validationSchema}
             onSubmit={(data, { setSubmitting }) => {
-              setSubmitting(true);
+              setSubmitting(true)
               // make async call
-              console.log("submit: ", data);
-              setSubmitting(false);
+              console.log('submit: ', data)
+              setSubmitting(false)
             }}
           >
             {({ values, errors, isSubmitting }) => (
               <Form>
-                <Typography variant="h6" gutterBottom>
+                <Typography variant='h6' gutterBottom>
                   General Information
                 </Typography>
                 <Grid container spacing={3}>
                   <Grid item xs={12}>
                     <Field
                       fullWidth
-                      placeholder="Title"
-                      name="title"
-                      type="input"
+                      placeholder='Title'
+                      name='title'
+                      type='input'
                       as={TextField}
                     />
                   </Grid>
-                  <Typography variant="h6" gutterBottom>
+                  <Typography variant='h6' gutterBottom>
                     Creators
                   </Typography>
                   <Grid item xs={12}>
-                    <FieldArray name="creators">
-                      {arrayHelpers => (
+                    <FieldArray name='creators'>
+                      {(arrayHelpers) => (
                         <div>
                           <Button
                             onClick={() =>
                               arrayHelpers.push({
-                                type: "frog",
-                                name: "",
-                                id: "" + Math.random()
+                                type: 'frog',
+                                name: '',
+                                id: '' + Math.random()
                               })
                             }
                           >
                             Add a Creator
-                        </Button>
+                          </Button>
                           {values.creators.map((creator, index) => {
                             return (
                               <div key={creator.id}>
-                                <MyTextField
-                                  placeholder="Name"
+                                <Field
+                                  placeholder='Name'
                                   name={`creators.${index}.name`}
+                                  type='select'
+                                  as={Select}
                                 />
                                 <Field
                                   name={`creators.${index}.type`}
-                                  type="select"
+                                  type='select'
                                   as={Select}
                                 >
-                                  <MenuItem value="cat">cat</MenuItem>
-                                  <MenuItem value="dog">dog</MenuItem>
-                                  <MenuItem value="frog">frog</MenuItem>
+                                  <MenuItem value='cat'>cat</MenuItem>
+                                  <MenuItem value='dog'>dog</MenuItem>
+                                  <MenuItem value='frog'>frog</MenuItem>
                                 </Field>
-                                <Button onClick={() => arrayHelpers.remove(index)}>
+                                <Button
+                                  onClick={() => arrayHelpers.remove(index)}
+                                >
                                   x
-                        </Button>
+                                </Button>
                               </div>
-                            );
+                            )
                           })}
                         </div>
                       )}
@@ -166,67 +158,68 @@ export const DatsCreatorGui = () => {
                   <Grid item xs={12}>
                     <Field
                       fullWidth
-                      placeholder="Description"
-                      name="description"
-                      type="input"
+                      placeholder='Description'
+                      name='description'
+                      type='input'
                       as={TextField}
                     />
                   </Grid>
                   <Grid item xs={12}>
                     <div>Types:</div>
                     <Field
-                      name="types"
-                      type="checkbox"
-                      value="type1"
+                      name='types'
+                      type='checkbox'
+                      value='type1'
                       as={Checkbox}
                     />
                     <Field
-                      name="types"
-                      type="checkbox"
-                      value="type2"
+                      name='types'
+                      type='checkbox'
+                      value='type2'
                       as={Checkbox}
                     />
                     <Field
-                      name="types"
-                      type="checkbox"
-                      value="type3"
-                      as={Checkbox} />
+                      name='types'
+                      type='checkbox'
+                      value='type3'
+                      as={Checkbox}
+                    />
                   </Grid>
                   <Grid item xs={12}>
                     <Field
                       fullWidth
-                      placeholder="License"
-                      name="license"
-                      type="input"
+                      placeholder='License'
+                      name='license'
+                      type='input'
                       as={TextField}
                     />
                   </Grid>
                   <Grid item xs={12}>
                     <Field
                       fullWidth
-                      placeholder="Keywords"
-                      name="keywords"
-                      type="input"
+                      placeholder='Keywords'
+                      name='keywords'
+                      type='input'
                       as={TextField}
                     />
                   </Grid>
 
                   <div>
-                    <Button disabled={isSubmitting} type="submit">
+                    <Button disabled={isSubmitting} type='submit'>
                       submit
                     </Button>
                   </div>
                 </Grid>
-                <Typography variant="h6" gutterBottom>
+                <Typography variant='h6' gutterBottom>
                   Next section
                 </Typography>
               </Form>
             )}
           </Formik>
         </Paper>
-        <pre>{JSON.stringify(values, null, 2)}</pre>
-        <pre>{JSON.stringify(errors, null, 2)}</pre>
+        {/* <pre>{JSON.stringify(values, null, 2)}</pre>
+        <pre>{JSON.stringify(errors, null, 2)}</pre> */}
       </div>
     </React.Fragment>
-  );
-};
+  )
+}
