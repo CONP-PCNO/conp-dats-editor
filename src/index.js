6fba80793e7e3212ad70f3723a02f71483535146
Liam O'Callaghan <liam@liamocallaghan.com>
2020-07-24 20:19:10 -0400

    add remaining fields

--- src/index.js
+++ src/index.js
@@ -123,7 +123,26 @@ export const DatsCreatorGui = () => {
                 value: '',
                 units: 'mb'
               },
-              authorizations: 'public'
+              authorizations: 'public',
+              files: '',
+              subjects: '',
+              conpStatus: '',
+              derivedFrom: '',
+              parentDatasetId: '',
+              primaryPublications: [],
+              dimensions: [],
+              identifier: '',
+              contact: '',
+              logo: '',
+              dates: [],
+              citations: [],
+              producedBy: '',
+              isAbout: '',
+              hasPart: '',
+              acknowledges: '',
+              refinement: '',
+              aggregation: '',
+              spacialCoverage: ''
             }}
             validationSchema={validationSchema}
             onSubmit={(data, { setSubmitting }) => {
@@ -404,6 +423,196 @@ export const DatsCreatorGui = () => {
                     <MenuItem value='registered'>Registered</MenuItem>
                     <MenuItem value='private'>Private</MenuItem>
                   </CustomSelectField>
+                </Grid>
+                <Grid item xs={12}>
+                  <Typography variant='h6' gutterBottom>
+                    Files
+                  </Typography>
+                  <CustomTextField fullWidth placeholder='Files' name='files' />
+                </Grid>
+                <Grid item xs={12}>
+                  <Typography variant='h6' gutterBottom>
+                    Subjects
+                  </Typography>
+                  <CustomTextField
+                    fullWidth
+                    placeholder='Subjects'
+                    name='subjects'
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <Typography variant='h6' gutterBottom>
+                    CONP status
+                  </Typography>
+                  <CustomTextField
+                    fullWidth
+                    placeholder='CONP status'
+                    name='conpStatus'
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <Typography variant='h6' gutterBottom>
+                    Derived From
+                  </Typography>
+                  <Grid item xs={6}>
+                    <CustomTextField
+                      fullWidth
+                      placeholder='Derived From'
+                      name='derivedFrom'
+                    />
+                  </Grid>
+                  <Grid item xs={6}>
+                    <CustomTextField
+                      fullWidth
+                      placeholder='Parent dataset id'
+                      name='parentDatasetId'
+                    />
+                  </Grid>
+                </Grid>
+                <Grid item xs={12}>
+                  <Typography variant='h6' gutterBottom>
+                    Primary Publications
+                  </Typography>
+                  <CustomTextField
+                    fullWidth
+                    placeholder='Primary Publications'
+                    name='primaryPublications'
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <Typography variant='h6' gutterBottom>
+                    Dimensions
+                  </Typography>
+                  <CustomTextField
+                    fullWidth
+                    placeholder='Dimensions'
+                    name='dimensions'
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <Typography variant='h6' gutterBottom>
+                    Identifier
+                  </Typography>
+                  <CustomTextField
+                    fullWidth
+                    placeholder='Identifier'
+                    name='identifier'
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <Typography variant='h6' gutterBottom>
+                    Contact
+                  </Typography>
+                  <CustomTextField
+                    fullWidth
+                    placeholder='Contact'
+                    name='contact'
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <Typography variant='h6' gutterBottom>
+                    Logo
+                  </Typography>
+                  <CustomTextField fullWidth placeholder='Logo' name='logo' />
+                </Grid>
+                <Grid item xs={12}>
+                  <Typography variant='h6' gutterBottom>
+                    Dates
+                  </Typography>
+                  <Grid item xs={6}>
+                    <CustomTextField
+                      fullWidth
+                      placeholder='Date'
+                      name='date.date'
+                    />
+                  </Grid>
+                  <Grid item xs={6}>
+                    <CustomTextField
+                      fullWidth
+                      placeholder='Description'
+                      name='date.description'
+                    />
+                  </Grid>
+                </Grid>
+                <Grid item xs={12}>
+                  <Typography variant='h6' gutterBottom>
+                    Citations
+                  </Typography>
+                  <CustomTextField
+                    fullWidth
+                    placeholder='Citations'
+                    name='citations'
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <Typography variant='h6' gutterBottom>
+                    Produced by
+                  </Typography>
+                  <CustomTextField
+                    fullWidth
+                    placeholder='Produced By'
+                    name='producedBy'
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <Typography variant='h6' gutterBottom>
+                    Is About
+                  </Typography>
+                  <CustomTextField
+                    fullWidth
+                    placeholder='Is About'
+                    name='isAbout'
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <Typography variant='h6' gutterBottom>
+                    Has Part
+                  </Typography>
+                  <CustomTextField
+                    fullWidth
+                    placeholder='Has Part'
+                    name='hasPart'
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <Typography variant='h6' gutterBottom>
+                    Acknowledges
+                  </Typography>
+                  <CustomTextField
+                    fullWidth
+                    placeholder='Acknowledges'
+                    name='acknowledges'
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <Typography variant='h6' gutterBottom>
+                    Refinement
+                  </Typography>
+                  <CustomTextField
+                    fullWidth
+                    placeholder='Refinement'
+                    name='refinement'
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <Typography variant='h6' gutterBottom>
+                    Aggregation
+                  </Typography>
+                  <CustomTextField
+                    fullWidth
+                    placeholder='Aggregation'
+                    name='aggregation'
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <Typography variant='h6' gutterBottom>
+                    Spacial Coverage
+                  </Typography>
+                  <CustomTextField
+                    fullWidth
+                    placeholder='Spacial Coverage'
+                    name='spacialCoverage'
+                  />
                 </Grid>
                 <div>
                   <Button disabled={isSubmitting} type='submit'>
