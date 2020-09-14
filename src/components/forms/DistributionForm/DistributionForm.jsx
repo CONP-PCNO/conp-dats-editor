2bd1e3a512a38e0445227fe186f23e7832d7490b
Liam O'Callaghan <liam@liamocallaghan.com>
2020-09-14 19:38:07 -0400

    refactor distribution page

--- src/components/forms/DistributionForm/DistributionForm.jsx
+++ src/components/forms/DistributionForm/DistributionForm.jsx
@@ -1,256 +1,110 @@
 import React from 'react'
-import {
-  Grid,
-  Typography,
-  Button,
-  MenuItem,
-  Divider,
-  Tooltip,
-  IconButton
-} from '@material-ui/core'
+import { Grid, Button, MenuItem, Divider, Box } from '@material-ui/core'
 import { FieldArray } from 'formik'
+import Section from '../../layout/Section'
+import SectionTitle from '../../layout/SectionTitle'
+import FieldGroup from '../../layout/FieldGroup'
 import CustomTextField from '../../fields/CustomTextField'
 import CustomSelectField from '../../fields/CustomSelectField'
-import InfoIcon from '@material-ui/icons/Info'
-import CancelIcon from '@material-ui/icons/Cancel'
 
 export default function GeneralForm(props) {
-  const { classes, values } = props
+  const { values } = props
   return (
     <React.Fragment>
-      <div className={classes.section}>
-        <Grid container spacing={3}>
-          <Grid item xs={12}>
-            <Grid container direction='row' spacing={1}>
-              <Grid item>
-                <Typography variant='h6' gutterBottom>
-                  Formats
-                </Typography>
-              </Grid>
-              <Grid item>
-                <Tooltip
-                  title='The formats included in the dataset'
-                  placement='right'
-                >
-                  <InfoIcon fontSize='small' color='action' />
-                </Tooltip>
-              </Grid>
-            </Grid>
-            <FieldArray name='formats'>
-              {(arrayHelpers) => (
-                <Grid container item spacing={5} xs={12}>
-                  {values.formats.map((format, index) => {
-                    return (
-                      <Grid
-                        key={'format_' + index}
-                        container
-                        item
-                        spacing={3}
-                        xs={12}
-                      >
-                        <Grid item xs={6}>
-                          <CustomTextField
-                            required
-                            fullWidth
-                            label='Format'
-                            name={`formats.${index}`}
-                          />
-                        </Grid>
-                        <Grid container item xs={3} justify='center'>
-                          {index !== 0 && (
-                            <IconButton
-                              color='default'
-                              onClick={() => arrayHelpers.remove(index)}
-                            >
-                              <CancelIcon />
-                            </IconButton>
-                          )}
-                        </Grid>
-                      </Grid>
-                    )
-                  })}
-                  <Grid item xs={6}>
-                    <Button
-                      variant='outlined'
-                      color='secondary'
-                      onClick={() => {
-                        arrayHelpers.push('')
-                      }}
-                    >
-                      Add another Format
-                    </Button>
-                  </Grid>
-                </Grid>
-              )}
-            </FieldArray>
-          </Grid>
-        </Grid>
-      </div>
-      <Divider variant='middle' />
-      <div className={classes.section}>
-        <Grid container spacing={3}>
-          <Grid item xs={12}>
-            <Grid container direction='row' spacing={1}>
-              <Grid item>
-                <Typography variant='h6' gutterBottom>
-                  Size
-                </Typography>
-              </Grid>
-              <Grid item>
-                <Tooltip title='Size of the Dataset' placement='right'>
-                  <InfoIcon fontSize='small' color='action' />
-                </Tooltip>
-              </Grid>
-            </Grid>
-            <Grid container item spacing={3} xs={12}>
-              <Grid item xs={9}>
-                <CustomTextField fullWidth label='Size' name='size.value' />
-              </Grid>
-              <Grid item xs={3}>
-                <CustomSelectField name='size.units' label='Units'>
-                  <MenuItem value='MB'>MB</MenuItem>
-                  <MenuItem value='GB'>GB</MenuItem>
-                  <MenuItem value='TB'>TB</MenuItem>
-                  <MenuItem value='PB'>PB</MenuItem>
-                </CustomSelectField>
-              </Grid>
-            </Grid>
-          </Grid>
-        </Grid>
-      </div>
-      <Divider variant='middle' />
-      <div className={classes.section}>
-        <Grid container spacing={3}>
-          <Grid item xs={12}>
-            <Grid container direction='row' spacing={1}>
-              <Grid item>
-                <Typography variant='h6' gutterBottom>
-                  Access
-                </Typography>
-              </Grid>
-              <Grid item>
-                <Tooltip title='How to Access the Dataset' placement='right'>
-                  <InfoIcon fontSize='small' color='action' />
-                </Tooltip>
-              </Grid>
-            </Grid>
-            <Grid container item spacing={3} xs={12}>
+      <Section>
+        <SectionTitle
+          name='Formats'
+          tooltip='The formats included in the dataset'
+        />
+        <FieldArray name='formats'>
+          {(arrayHelpers) => (
+            <Box display='flex flex-column'>
+              {values.formats.map((format, index) => {
+                return (
+                  <FieldGroup
+                    key={'format_' + index}
+                    index={index}
+                    arrayHelpers={arrayHelpers}
+                  >
+                    <CustomTextField
+                      required
+                      fullWidth
+                      label='Format'
+                      name={`formats.${index}`}
+                    />
+                  </FieldGroup>
+                )
+              })}
               <Grid item xs={6}>
-                <CustomTextField
-                  fullWidth
-                  label='Landing Page'
-                  name='access.landingPage'
-                />
-              </Grid>
-              <Grid item xs={6}>
-                <CustomSelectField
-                  fullWidth
-                  label='Authorization'
-                  name='access.authorization'
+                <Button
+                  variant='outlined'
+                  color='secondary'
+                  onClick={() => {
+                    arrayHelpers.push('')
+                  }}
                 >
-                  <MenuItem value='public'>Public</MenuItem>
-                  <MenuItem value='registered'>Registered</MenuItem>
-                  <MenuItem value='private'>Private</MenuItem>
-                </CustomSelectField>
+                  Add another Format
+                </Button>
               </Grid>
-            </Grid>
-          </Grid>
-        </Grid>
-      </div>
+            </Box>
+          )}
+        </FieldArray>
+      </Section>
       <Divider variant='middle' />
-      <div className={classes.section}>
-        <Grid container spacing={3}>
-          <Grid item xs={12}>
-            <Grid container direction='row' spacing={1}>
-              <Grid item>
-                <Typography variant='h6' gutterBottom>
-                  Privacy
-                </Typography>
-              </Grid>
-              <Grid item>
-                <Tooltip title='Dataset Privacy' placement='right'>
-                  <InfoIcon fontSize='small' color='action' />
-                </Tooltip>
-              </Grid>
-            </Grid>
-            <CustomSelectField name='privacy' label='Privacy'>
-              <MenuItem value='public'>Public</MenuItem>
-              <MenuItem value='registered'>Registered</MenuItem>
-              <MenuItem value='private'>Private</MenuItem>
-            </CustomSelectField>
-          </Grid>
-        </Grid>
-      </div>
+      <Section>
+        <SectionTitle name='Size' tooltip='Size of the Dataset' />
+        <CustomTextField label='Size' name='size.value' />
+        <CustomSelectField name='size.units' label='Units'>
+          <MenuItem value='MB'>MB</MenuItem>
+          <MenuItem value='GB'>GB</MenuItem>
+          <MenuItem value='TB'>TB</MenuItem>
+          <MenuItem value='PB'>PB</MenuItem>
+        </CustomSelectField>
+      </Section>
       <Divider variant='middle' />
-      <div className={classes.section}>
-        <Grid container spacing={3}>
-          <Grid item xs={12}>
-            <Grid container direction='row' spacing={1}>
-              <Grid item>
-                <Typography variant='h6' gutterBottom>
-                  Number of Files
-                </Typography>
-              </Grid>
-              <Grid item>
-                <Tooltip
-                  title='Number of files in the dataset'
-                  placement='right'
-                >
-                  <InfoIcon fontSize='small' color='action' />
-                </Tooltip>
-              </Grid>
-            </Grid>
-            <CustomTextField fullWidth label='Files' name='files' />
-          </Grid>
-        </Grid>
-      </div>
+      <Section>
+        <SectionTitle name='Access' tooltip='How to access the dataset' />
+        <CustomTextField label='Landing Page' name='access.landingPage' />
+        <CustomSelectField label='Authorization' name='access.authorization'>
+          <MenuItem value='public'>Public</MenuItem>
+          <MenuItem value='registered'>Registered</MenuItem>
+          <MenuItem value='private'>Private</MenuItem>
+        </CustomSelectField>
+      </Section>
       <Divider variant='middle' />
-      <div className={classes.section}>
-        <Grid container spacing={3}>
-          <Grid item xs={12}>
-            <Grid container direction='row' spacing={1}>
-              <Grid item>
-                <Typography variant='h6' gutterBottom>
-                  Number Subjects
-                </Typography>
-              </Grid>
-              <Grid item>
-                <Tooltip
-                  title='Number of subjects in the dataset'
-                  placement='right'
-                >
-                  <InfoIcon fontSize='small' color='action' />
-                </Tooltip>
-              </Grid>
-            </Grid>
-            <CustomTextField fullWidth label='Subjects' name='subjects' />
-          </Grid>
-        </Grid>
-      </div>
+      <Section>
+        <SectionTitle name='Privacy' tooltip='Dataset Privacy' />
+        <CustomSelectField name='privacy' label='Privacy'>
+          <MenuItem value='public'>Public</MenuItem>
+          <MenuItem value='registered'>Registered</MenuItem>
+          <MenuItem value='private'>Private</MenuItem>
+        </CustomSelectField>
+      </Section>
       <Divider variant='middle' />
-      <div className={classes.section}>
-        <Grid container spacing={3}>
-          <Grid item xs={12}>
-            <Grid container direction='row' spacing={1}>
-              <Grid item>
-                <Typography variant='h6' gutterBottom>
-                  CONP status
-                </Typography>
-              </Grid>
-              <Grid item>
-                <Tooltip title='CONP status' placement='right'>
-                  <InfoIcon fontSize='small' color='action' />
-                </Tooltip>
-              </Grid>
-            </Grid>
-            <CustomSelectField name='conpStatus' label='CONP Status'>
-              <MenuItem value='conp'>CONP</MenuItem>
-              <MenuItem value='canadian'>Canadian</MenuItem>
-              <MenuItem value='external'>External</MenuItem>
-            </CustomSelectField>
-          </Grid>
-        </Grid>
-      </div>
+      <Section>
+        <SectionTitle
+          name='Number of Files'
+          tooltip='The number of files in the dataset'
+        />
+        <CustomTextField label='Files' name='files' />
+      </Section>
+      <Divider variant='middle' />
+      <Section>
+        <SectionTitle
+          name='Number of Subjects'
+          tooltip='The number of subjects in the dataset'
+        />
+        <CustomTextField label='Subjects' name='subjects' />
+      </Section>
+      <Divider variant='middle' />
+      <Section>
+        <SectionTitle name='CONP Status' tooltip='CONP Status' />
+        <CustomSelectField name='conpStatus' label='CONP Status'>
+          <MenuItem value='conp'>CONP</MenuItem>
+          <MenuItem value='canadian'>Canadian</MenuItem>
+          <MenuItem value='external'>External</MenuItem>
+        </CustomSelectField>
+      </Section>
     </React.Fragment>
   )
 }
